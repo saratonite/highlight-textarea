f6f61845fdd1ce445605971d85dfbd128dcd8b5a
Sarath <sarath.tk@icloud.com>
2020-08-27 11:34:34 +0530

    non latin word boundry issue fixed

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,22 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { HighlightTextArea } from '../.';
 
 const App = () => {
+
+  const [text, setText] = React.useState('red,green,blue,മലയാളം,⚡️⚡️⚡️')
   return (
-    <div> 
-      <h1>Type Hashtags : </h1>
-      
-      <Thing />
+    <div className="max-w-xl mx-auto"> 
+      <h1 className="text-3xl text-teal-300">Highight Text Area Component </h1>
+      <div className="my-2 flex items-center">
+
+      <label className="text-blue-500">Highight words : </label>
+      <input className="px-2 py-1 text-lg border border-indigo-400 rounded-ml text-red-400 flex-grow " value={text} onChange={e => setText(e.target.value)} />
+      </div>
+      <HighlightTextArea
+      highlightWords={text.split(',').map(t => t.trim())}
+      defaultValue="red flowers, blue sky, green trees മലയാളം ⚡️⚡️⚡️ ⚡️⚡️⚡️⚡️⚡️⚡️"/>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,28 @@
 import * as React from 'react';
 
+
+function compileText(text:string, words:string[]): string {
+
+  let input:string = text;
+  input = input.replace(/\n(\{\{\}\})?$/, '\n\n$1');
+
+ // encode HTML entities
+ input = input.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+ 
+  words.forEach((word) => {
+   //let reg = new RegExp('\\b'+word+'\\b','g'); // The word boundry (\b) not working with non latin chars
+   let reg = new RegExp('(?<=[\\s,.:;"\']|^)'+ word +'(?=[\\s,.:;"\']|$)','g');
+   input = input.replace(reg, '<span class="ht-spell" onclick="alert(0)">'+word+'</span>');
+
+  })
+
+  return input
+}
+
+let hWords: string[]= []
+
 // Delete me
-export const Thing = () => {
+export const HighlightTextArea = ({defaultValue = '', highlightWords = []}) => {
 
   const ghost = React.useRef<HTMLDivElement>(null)
   const editor= React.useRef<HTMLTextAreaElement>(null)
@@ -12,43 +33,53 @@ export const Thing = () => {
 
     if(backdrop.current) {
       backdrop.current.scrollTop = editor.current?.scrollTop || 0;
-      ///ghost.current.style = editor.current?.style
+    
     }
   }
+
+
 
   React.useEffect(() => {
     if(editor.current){
       editor.current.addEventListener('scroll', syncScroll, false)
-      //editor.current.addEventListener('input', syncScroll, false)
+      editor.current.addEventListener('input', (e:any) => {
+        const { value } = e.target
+
+        if(ghost.current) {
+          ghost.current.innerHTML = compileText(value, hWords)
+        }
+      }, false)
+    }
+
+    // set Default value
+
+    if(ghost.current) {
+      
+      ghost.current.innerHTML = compileText(defaultValue, highlightWords)
     }
 
   
   },[])
+
+
+  React.useEffect(() => {
+
+    if(ghost.current && editor.current) {
+      ghost.current.innerHTML = compileText(editor.current?.value, highlightWords)
+      hWords = highlightWords
+    }
+
+  },[highlightWords])
     
   return (
-  <div style={{ position: "relative", width:"100%", height:"300px", border:"2px solid blue", overflowX:"hidden", overflowY:"hidden"}}>
-    <div ref={backdrop} className="backdrop">
-      <div className='ghost' ref={ghost}>e</div>
+  <div className="ht-containers" >
+    <div ref={backdrop} className="ht-backdrop">
+      <div className='ht-ghost' ref={ghost}>e</div>
     </div>
     
-    <textarea lang="ml" spellCheck={false}  onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-       //console.log(e.target.value)
-       if(ghost.current) {
-
-        const { value } = e.target
-        let input:string = value;
-         input = input.replace(/\n(\{\{hwt-mark-stop\}\})?$/, '\n\n$1');
-
-        // encode HTML entities
-			  input = input.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-
-        input = input.replace(/#([^ \n]+)/g, '<span class="spell">#$1</span>');
-        
-
-        //console.log('Prsed', newLineParse)
-        
-        ghost.current.innerHTML = input
-      }
-    }} ref={editor} style={{}}></textarea>
+    <textarea defaultValue={defaultValue} 
+      className="ht-textarea"
+      spellCheck={false} 
+     ref={editor} style={{}}></textarea>
   </div>)
 };
